8bb3b1a8d6e7ef5b13de50aab2d1cae2a2cb49b3
Anurag Mishra <anurag.mishra@eximietas.design>
2024-07-30 16:39:36 +0530

    fix(ui): bottom navigation icons

--- src/components/common/BottomNavigation.jsx
+++ src/components/common/BottomNavigation.jsx
@@ -14,28 +14,27 @@ const BottomNavigation = (props) => {
   return (
     <div className="bottom-navigation">
       <ul className="bottom-navigation-menu">
-        <li><NavLink exact activeClassName="active" to={ROUTE.HOME}><HomeOutlined style={{fontSize: '35px'}}/>Home</NavLink></li>
-        <li><NavLink activeClassName="active" to={ROUTE.SHOP}><ShoppingOutlined style={{fontSize: '35px'}}/>Shop</NavLink></li>
+        <li><NavLink exact activeClassName="active" to={ROUTE.HOME} style={{fontSize: '13px'}}><HomeOutlined style={{fontSize: '26px'}}/>Home</NavLink></li>
+        <li><NavLink activeClassName="active" to={ROUTE.SHOP} style={{fontSize: '13px'}}><ShoppingOutlined style={{fontSize: '26px'}}/>Shop</NavLink></li>
         <li>
           <BasketToggle>
             {({ onClickToggle }) => (
               <button
                 className="button-link navigation-menu-link basket-toggle"
-                style={{ color: '#FF8C00'}}
                 disabled={disabledPaths.includes(pathname)}
                 onClick={onClickToggle}
                 type="button"
               >
                 <Badge count={basketLength}>
-                  <ShoppingCartOutlined style={{ fontSize: "40px" }} />
-                  Cart
+                  <ShoppingCartOutlined style={{ fontSize: "35px", color: '#FF8C00' }} />
+                  <span style={{ fontSize: "12px", color: '#FF8C00' }}>Cart</span>
                 </Badge>
               </button>
             )}
           </BasketToggle>
         </li>
-        <li><NavLink activeClassName="active" to={ROUTE.FEATURED_PRODUCTS}><ThunderboltOutlined style={{fontSize: '35px'}}/>Feat</NavLink></li>
-        <li><NavLink activeClassName="active" to={ROUTE.RECOMMENDED_PRODUCTS}><CheckCircleOutlined style={{fontSize: '35px'}}/>Assured</NavLink></li>
+        <li><NavLink activeClassName="active" to={ROUTE.FEATURED_PRODUCTS} style={{fontSize: '13px'}}><ThunderboltOutlined style={{fontSize: '26px'}}/>Feat</NavLink></li>
+        <li><NavLink activeClassName="active" to={ROUTE.RECOMMENDED_PRODUCTS} style={{fontSize: '13px'}}><CheckCircleOutlined style={{fontSize: '26px'}}/>Assured</NavLink></li>
       </ul>
     </div>
   );
